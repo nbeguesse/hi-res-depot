6d232fa5bd161099bf2de237e9144d40c0ad45ec
Nicole Beguesse <nicole@highlinesale.com>
2016-08-25 11:15:26 -0400

    Adding s3 config for production

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,6 +3,6 @@ class Image < ActiveRecord::Base
     has_attached_file :file, 
       styles: { thumb: "200x200>" }, 
       default_url: "/images/:style/missing.png",
-      storage: "filesystem"
+      storage: Rails.env.production? ? "s3" : "filesystem"
     validates_attachment_content_type :file, :content_type => /\Aimage\/.*\Z/
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('S3_KEY'),
+      secret_access_key: ENV.fetch('S3_SECRET'),
+      s3_region: 'us-east-1',
+    }
+  }
 end
